bc4b10ceff91a0c5976a73b8e11382b980249662
03034844244 <abdulhadi20032922@gmail.com>
2023-12-09 10:26:41 +0500

    deliverable 5

---
+++ src/main/java/com/jtspringproject/JtSpringProject/dao/userDao.java
@@ -0,0 +1,77 @@
+package com.jtspringproject.JtSpringProject.dao;
+
+import java.util.List;
+
+import javax.persistence.NoResultException;
+import javax.sound.midi.Soundbank;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jtspringproject.JtSpringProject.models.User;
+
+
+@Repository
+public class userDao {
+	@Autowired
+    private SessionFactory sessionFactory;
+	
+	public void setSessionFactory(SessionFactory sf) {
+        this.sessionFactory = sf;
+    }
+   @Transactional
+    public List<User> getAllUser() {
+        Session session = this.sessionFactory.getCurrentSession();
+		List<User>  userList = session.createQuery("from CUSTOMER").list();
+        return userList;
+    }
+    
+    @Transactional
+	public User saveUser(User user) {
+		this.sessionFactory.getCurrentSession().saveOrUpdate(user);
+		System.out.println("User added" + user.getId());
+        return user;
+	}
+    
+//    public User checkLogin() {
+//    	this.sessionFactory.getCurrentSession().
+//    }
+    @Transactional
+    public User getUser(String username,String password) {
+    	Query query = sessionFactory.getCurrentSession().createQuery("from CUSTOMER where username = :username");
+    	query.setParameter("username",username);
+    	
+    	try {
+			User user = (User) query.getSingleResult();
+			System.out.println(user.getPassword());
+			if(password.equals(user.getPassword())) {
+				return user;
+			}else {		
+				return new User();
+			}
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+			User user = new User();
+			return user;
+		}
+    	
+    }
+	@Transactional
+	public boolean deleteUser(int id) {
+		try (Session session = this.sessionFactory.getCurrentSession()) {
+			User userToDelete = session.get(User.class, id);
+
+			if (userToDelete != null) {
+				session.delete(userToDelete);
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace(); // Log the exception or handle it as appropriate
+		}
+		return false;
+	}
+}
